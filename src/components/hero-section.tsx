8f33202769ee3157b992206a7aa5c24b6d2413da
Ahmed Magdy <ahmed.magdy.1138@gmail.com>
2024-10-06 17:11:30 +0300

    Refactor HeroSection component to use Next.js Image component
    Replace the `<img>` tag with Next.js `<Image>` component in the HeroSection to optimize image loading and improve performance. The new component includes `layout="fill"` and `priority={true}` attributes to ensure the image is loaded with high priority and covers the entire section. Additionally, fix an apostrophe issue in the introduction text.

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -1,10 +1,14 @@
+import Image from "next/image";
+
 export default function HeroSection() {
   return (
     <section className="flex-center relative h-screen bg-gradient-to-b from-[#000218] to-[#0a0a0a]">
-      <img
-        className="animate-fade pointer-events-none absolute left-0 top-0 h-full object-cover"
+      <Image
         src="/imgs/spotlight.png"
         alt="Spotlight"
+        layout="fill"
+        priority={true}
+        className="animate-fade pointer-events-none absolute left-0 top-0 h-full object-cover"
       />
       <div className="flex-center w-full flex-col gap-6 px-4">
         <span className="text-center text-xs uppercase tracking-widest text-gray-300">Dynamic Web Magic with Next.js</span>
@@ -12,7 +16,7 @@ export default function HeroSection() {
           Transforming Concepts into Seamless <span className="text-purple-300">User Experiences</span>
         </h1>
         <p className="text-center text-sm text-gray-300 sm:text-base md:text-lg">
-          Hi! I'm <strong className="font-medium text-white">Ahmed Magdy</strong>, a Next.js developer.
+          Hi! I&apos;m <strong className="font-medium text-white">Ahmed Magdy</strong>, a Next.js developer.
         </p>
         <button className="flex items-center justify-center rounded-xl border border-gray-800 bg-gray-950 px-6 py-3 text-white transition-colors duration-[350ms] hover:bg-gray-800">
           Show my work
